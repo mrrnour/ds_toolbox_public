4ca453f1a4fe0e62d83357629f9eafc8615097e7
mrrnour <reza.nourzadeh@gmail.com>
2024-11-18 21:36:03 +0000

    clean init

--- dsToolbox/__init__.py
+++ dsToolbox/__init__.py
@@ -5,14 +5,14 @@
 # from dsToolbox import spark_funcs
 # from dsToolbox import config
 # from dsToolbox import sql_template
-from dsToolbox import default_values
-from importlib import resources as res
-import yaml
+# from dsToolbox import default_values
+# from importlib import resources as res
+# import yaml
 
-with res.open_binary('dsToolbox', 'config.yml') as fp:
-    config_dict = yaml.load(fp, Loader=yaml.Loader)
+# with res.open_binary('dsToolbox', 'config.yml') as fp:
+#     config_dict = yaml.load(fp, Loader=yaml.Loader)
     
-with res.open_binary('dsToolbox', 'sql_template.yml') as fp:
-    sql_template_dict = yaml.load(fp, Loader=yaml.Loader)
+# with res.open_binary('dsToolbox', 'sql_template.yml') as fp:
+#     sql_template_dict = yaml.load(fp, Loader=yaml.Loader)
 
-print('dsToolbox was successfully imported')
+# print('dsToolbox was successfully imported')
